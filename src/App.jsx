f5aad97cd330538b9db70323582a4d39451d31f7
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-04-11 12:43:41 +0530

    update: Text and background

--- src/App.jsx
+++ src/App.jsx
@@ -5,64 +5,83 @@ import Card3 from "./cards/Card3";
 import Card4 from "./cards/Card4";
 import Card5 from "./cards/Card5";
 import "./App.css";
-import CanvasBackground from "./CanvasBackground";
+import CanvasBackground from "./CanvasBackground.jsx";
 
 const App = ({ senior }) => {
-	const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-	const handleNext = () => {
-		if (page < 5) {
-			console.log("next");
-			setPage((prev) => prev + 1);
-		}
-	};
+  const handleNext = () => {
+    if (page < 5) {
+      console.log("next");
+      setPage((prev) => prev + 1);
+    }
+  };
 
-	const handlePrev = () => {
-		if (page > 1) {
-			setPage((prev) => prev - 1);
-		}
-	};
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
 
-	const cards = [
-		<Card5 page={page} handleNext={handleNext} handlePrev={handlePrev} key={5} senior={senior} />,
-		<Card4 page={page} handleNext={handleNext} handlePrev={handlePrev} key={4} senior={senior} />,
-		<Card3 page={page} handleNext={handleNext} handlePrev={handlePrev} key={3} senior={senior} />,
-		<Card2 page={page} handleNext={handleNext} handlePrev={handlePrev} key={2} senior={senior} />,
-		<Card1 page={page} handleNext={handleNext} handlePrev={handlePrev} key={1} senior={senior} />,
-	];
+  const cards = [
+    <Card5
+      page={page}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      key={5}
+      senior={senior}
+    />,
+    <Card4
+      page={page}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      key={4}
+      senior={senior}
+    />,
+    <Card3
+      page={page}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      key={3}
+      senior={senior}
+    />,
+    <Card2
+      page={page}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      key={2}
+      senior={senior}
+    />,
+    <Card1
+      page={page}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      key={1}
+      senior={senior}
+    />,
+  ];
 
-	useEffect(() => {}, [page]);
+  useEffect(() => {}, [page]);
 
-	return (
-		<main
-			className={`w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-[url("/bg.png")] bg-no-repeat bg-cover`}>
-			{page > 1 && (
-				<button
-					onClick={handlePrev}
-					className="bg-gray-500 text-white px-3 py-1 rounded-md relative -top-6">
-					Previous
-				</button>
-			)}
-			<div id="book" className="relative w-80 h-[500px]">
-				{cards}
-			</div>
-		</main>
-	);
-	return (
-		<main className={`w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-[#1c1e1e]`}>
-			<CanvasBackground />
-			{page > 1 && (
-				<button
-					onClick={handlePrev}
-					className="bg-gray-500 text-white px-3 py-1 rounded-md relative -top-6">
-					Previous
-				</button>
-			)}
-			<div id="book" className="relative w-80 h-[500px] z-10">
-				{cards}
-			</div>
-		</main>
-	);
+  return (
+    <main
+      className={`w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-[#1c1e1e]`}
+    >
+      <CanvasBackground />
+
+      {page > 1 && (
+        <button
+          onClick={handlePrev}
+          className="bg-gray-500 text-white px-3 py-1 rounded-md relative -top-6 z-10"
+        >
+          Previous
+        </button>
+      )}
+      <div id="book" className="relative w-80 h-[500px] z-10">
+        {cards}
+      </div>
+    </main>
+  );
 };
 
 export default App;
--- src/cards/Card2.jsx
+++ src/cards/Card2.jsx
@@ -1,57 +1,63 @@
 import React from "react";
 
 const Card2 = ({ page, handleNext, senior, handlePrev }) => {
-	return (
-		<div className={`perspective-2 ${page > 2 && "w-0 h-0"} absolute`}>
-			<div
-				className={` transition-all duration-1000 origin-left ${page > 2 && "rotate-y-135"} ${
-					page == 2 && "rotate-y-20"
-				} overflow-hidden w-80 h-[500px] relative bg-[#fce501] flex flex-col justify-center items-center`}
-				onClick={() => {
-					page > 2 && handlePrev();
-				}}>
-				{page == 2 && (
-					<>
-						<div className="text-[#fd7196] font-mono">
-							<span className="font-bold text-lg"> COPS </span> <span> Farewell 2k24 </span>
-						</div>
+  return (
+    <div className={`perspective-2 ${page > 2 && "w-0 h-0"} absolute`}>
+      <div
+        className={` transition-all duration-1000 origin-left ${
+          page > 2 && "rotate-y-135"
+        } ${
+          page == 2 && "rotate-y-20"
+        } overflow-hidden w-80 h-[500px] relative bg-[#fce501] flex flex-col justify-center items-center`}
+        onClick={() => {
+          page > 2 && handlePrev();
+        }}
+      >
+        {page == 2 && (
+          <>
+            <div className="text-[#fd7196] font-mono">
+              <span className="font-bold text-lg"> COPS </span>{" "}
+              <span> Farewell 2k24 </span>
+            </div>
 
-						<div className="pl-5 pr-3 relative mb-2">
-							<div className="flex flex-col">
-								<span
-									className="
+            <div className="pl-5 pr-3 relative mb-2">
+              <div className="flex flex-col">
+                <span
+                  className="
                     text-[#fd7196]
-                  text-5xl space-x-3 font-bold font-sans">
-									The coolest techie of the year goes to
-								</span>
-								<span className="text-[#fd7196] text-lg">{senior.name}</span>
-							</div>
-						</div>
+                  text-4xl space-x-3 font-bold font-sans mb-1"
+                >
+                  The coolest techie of the year goes to
+                </span>
+                <span className="text-[#fd7196] text-lg">{senior.name}</span>
+              </div>
+            </div>
 
-						<div className="w-full px-3 flex flex-col items-center justify-between relative">
-							<div className="relative">
-								<div className="bg-[#fc618a] w-32 h-32 rounded-full absolute -top-16 -right-16"></div>
-								<div className="bg-[#fce501] w-16 h-16 rounded-bl-full absolute top-0 right-0"></div>
+            <div className="w-full px-3 flex flex-col items-center justify-between relative">
+              <div className="relative">
+                <div className="bg-[#fc618a] w-32 h-32 rounded-full absolute -top-16 -right-16"></div>
+                <div className="bg-[#fce501] w-16 h-16 rounded-bl-full absolute top-0 right-0"></div>
 
-								<div className="bg-[#fc618a] w-16 h-16 rounded-full absolute -bottom-8 -right-8"></div>
-								<div className="bg-[#fce501] w-8 h-8 rounded-tl-full absolute bottom-0 right-0"></div>
+                <div className="bg-[#fc618a] w-16 h-16 rounded-full absolute -bottom-8 -right-8"></div>
+                <div className="bg-[#fce501] w-8 h-8 rounded-tl-full absolute bottom-0 right-0"></div>
 
-								<div className="bg-[#fc618a] w-32 h-32 rounded-full absolute -bottom-16 -left-16"></div>
-								<div className="bg-[#fce501] w-16 h-16 rounded-tr-full absolute bottom-0 left-0"></div>
+                <div className="bg-[#fc618a] w-32 h-32 rounded-full absolute -bottom-16 -left-16"></div>
+                <div className="bg-[#fce501] w-16 h-16 rounded-tr-full absolute bottom-0 left-0"></div>
 
-								<img src="/face.jpg" className="w-full h-full mb-2 z-10" />
-							</div>
-							<button
-								onClick={handleNext}
-								className="bg-[#fc618a] text-[#fce501] px-3 py-1 rounded-md w-20 h-8 m-2">
-								Next
-							</button>
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+                <img src="/face.jpg" className="w-full h-full mb-2 z-10" />
+              </div>
+              <button
+                onClick={handleNext}
+                className="bg-[#fc618a] text-[#fce501] px-3 py-1 rounded-md w-20 h-8 m-2"
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Card2;
