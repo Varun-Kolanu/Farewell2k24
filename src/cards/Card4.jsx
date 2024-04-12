dbb2d4ce03aaec5debe47c17ca6aa881e1896c4c
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-04-13 01:58:28 +0530

    add: title and minor last page links

--- src/cards/Card4.jsx
+++ src/cards/Card4.jsx
@@ -2,49 +2,60 @@ import React from "react";
 import { IoOpenOutline } from "react-icons/io5";
 
 const Card4 = ({ page, handleNext, handlePrev }) => {
-	return (
-		<div className={`perspective-4 ${page > 4 && "w-0 h-0"} absolute`}>
-			<div
-				className={`pt-2 transition-all duration-1000 origin-left ${page > 4 && "rotate-y-135"} ${
-					page == 4 && "rotate-y-20"
-				} overflow-hidden w-80 h-[500px] relative bg-[#fc618a] flex flex-col justify-start items-center`}
-				onClick={() => {
-					page > 4 && handlePrev();
-				}}>
-				{page == 4 && (
-					<>
-						<div className="text-[#fce501] font-mono">
-							<span className="font-bold text-lg"> COPS </span> <span> Farewell 2k24 </span>
-						</div>
-						<div className="pl-5 pr-3 relative mb-2">
-							<div className="flex flex-col">
-								<div className="text-4xl space-x-3 font-bold font-sans">
-									<span className="text-[#fce501]"> Address: Tanish Cafe </span>
-									<a
-										href="https://maps.app.goo.gl/ersVG3wZDZdH1WbT7"
-										className="inline-block ml-4 relative top-1 text-blue-700"
-										target="_blank">
-										<IoOpenOutline />
-									</a>
-								</div>
-								<span className="text-[#fce501] text-lg">at 6 PM, 17 April</span>
-							</div>
-						</div>
+  return (
+    <div className={`perspective-4 ${page > 4 && "w-0 h-0"} absolute`}>
+      <div
+        className={`pt-2 transition-all duration-1000 origin-left ${
+          page > 4 && "rotate-y-135"
+        } ${
+          page == 4 && "rotate-y-20"
+        } overflow-hidden w-80 h-[500px] relative bg-[#fc618a] flex flex-col justify-start items-center`}
+        onClick={() => {
+          page > 4 && handlePrev();
+        }}
+      >
+        {page == 4 && (
+          <>
+            <div className="text-[#fce501] font-mono">
+              <span className="font-bold text-lg"> COPS </span>{" "}
+              <span> Farewell 2k24 </span>
+            </div>
+            <div className="pl-5 pr-3 relative mb-2">
+              <div className="flex flex-col">
+                <div className="text-4xl space-x-3 font-bold font-sans">
+                  <span className="text-[#fce501]"> Address: Tanish Cafe </span>
+                  <a
+                    href="https://maps.app.goo.gl/ersVG3wZDZdH1WbT7"
+                    className="inline-block ml-4 relative top-1 text-[#1b97bf]"
+                    target="_blank"
+                  >
+                    <IoOpenOutline />
+                  </a>
+                </div>
+                <span className="text-[#fce501] text-lg">
+                  at 6 PM, 17 April
+                </span>
+              </div>
+            </div>
 
-						<button
-							onClick={handleNext}
-							className="bg-[#fce501] text-[#fc618a] px-3 py-1 rounded-md w-20 h-8 m-2">
-							Next
-						</button>
+            <button
+              onClick={handleNext}
+              className="bg-[#fce501] text-[#fc618a] px-3 py-1 rounded-md w-20 h-8 m-2 absolute bottom-2 z-30"
+            >
+              Next
+            </button>
 
-						<div className="relative w-full h-full">
-							<img src="/party.jpg" className="rounded-t-full w-[500px] h-full absolute bottom-0" />
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+            <div className="relative w-full h-full">
+              <img
+                src="/party.jpg"
+                className="rounded-t-full w-[500px] h-full absolute bottom-0"
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Card4;
--- src/cards/Card7.jsx
+++ src/cards/Card7.jsx
@@ -1,35 +1,56 @@
 import React from "react";
+import { FaApple, FaAndroid } from "react-icons/fa";
 
-const Card7 = ({ page, handlePrev }) => {
-	return (
-		<div className={`perspective-1 ${page > 7 && "w-0 h-0"} absolute`}>
-			<div
-				className={` transition-all duration-1000 origin-left overflow-hidden w-80 h-[500px] relative bg-[#f95d42] flex flex-col justify-center items-center`}
-				onClick={() => {
-					page > 7 && handlePrev();
-				}}>
-				{page == 7 && (
-					<>
-						<div className="p-9 ">
-							<div className="text-5xl space-x-3 font-bold font-sans z-10 relative top-2 left-2 ">
-								<span
-									className="
-                  text-green-400
+const Card7 = ({ page, handleNext, handlePrev }) => {
+  return (
+    <div className={`perspective-1 ${page > 7 && "w-0 h-0"} absolute`}>
+      <div
+        className={` transition-all duration-1000 origin-left overflow-hidden w-80 h-[500px] relative bg-[#f95d42] flex flex-col justify-center items-center`}
+        onClick={() => {
+          page > 7 && handlePrev();
+        }}
+      >
+        {page == 7 && (
+          <>
+            <div className="p-9 ">
+              <div className="text-3xl space-x-3 font-bold font-sans z-10 relative ">
+                <span
+                  className="
+                  text-[#c5efc9]
                   relative -left-3
-                  [word-spacing:5px]">
-									Download this app to know more.
-								</span>
-							</div>
-						</div>
+                  [word-spacing:5px]"
+                >
+                  Unlock the magical surprise with your invitation!
+                </span>
+              </div>
+            </div>
 
-						<div className="absolute bottom-4 text-red-500 font-sans">
-							<span className="font-bold text-lg"> COPS </span> <span> Farewell 2k24 </span>
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+            <div className="w-full flex justify-between px-2 py-3 underline mt-2 text-xl text-[#c5efc9]">
+              <a href="" className="flex items-center">
+                <FaApple className="inline-block text-2xl" />
+                <span>iOS</span>
+              </a>
+              <a href="">
+                <FaAndroid className="inline-block text-2xl mr-1" />
+                Android
+              </a>
+            </div>
+
+            <ol className="text-[#c5efc9]">
+              <li>1. Download and install the app</li>
+              <li>2. Open & scan your invitation</li>
+              <li>3. Enjoy the Surprise!</li>
+            </ol>
+
+            <div className="absolute bottom-4 text-[#c5efc9] font-sans">
+              <span className="font-bold text-lg"> COPS </span>{" "}
+              <span> Farewell 2k24 </span>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Card7;
